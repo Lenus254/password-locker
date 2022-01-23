6c0a82c251910b5a694ac76b17262864f7520260
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-01-23 07:19:14 +0300

    added test init function to test if an object initialized properly

--- user_test.py
+++ user_test.py
@@ -2,13 +2,31 @@ import unittest
 from user import User
 
 class TestUser(unittest.TestCase):
-    """define test case for user class behavior"""
 
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    '''
     def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
-    def tearDown(self):
-        User.user_list = []
+        self.new_user = User("linus","lin","lin@lin","12345") # create user object
 
-         self.new_user = User("Linus","lin","lin@lin","12345") # create user object for test
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the user object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"linus")
+        self.assertEqual(self.new_user.last_name,"lin")
+        self.assertEqual(self.new_user.user_name,"lin@lin")
+        self.assertEqual(self.new_user.password,"12345")
