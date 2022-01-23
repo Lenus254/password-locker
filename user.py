f9b7bb2102b2778075670eb48e16fc32ebdeeb63
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-01-23 09:38:58 +0300

    created a test function to test if the new user is appended to the list

--- user.py
+++ user.py
@@ -3,13 +3,13 @@ class User:
     Class that generates new instances of users.
     """
 
-user_list=[] #Empty list to store users
+    user_list=[] #Empty list to store users
 
-def __init__(self,first_name,last_name,user_name,password):
+    def __init__(self,first_name,last_name,user_name,password):
 
-    """instance of a class"""
+        """instance of a class"""
     
-    self.first_name = first_name
-    self.last_name = last_name
-    self.user_number = user_name
-    self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
--- user_test.py
+++ user_test.py
@@ -1,11 +1,11 @@
-import unittest
-from user import User
+import unittest # Importing the unittest module
+from user import User # Importing the User class
+# import pyperclip
 
 class TestUser(unittest.TestCase):
 
     '''
-    Test class that defines test cases for the user class behaviours.
-
+    Test class that defines test cases for the User class behaviours.
     '''
     def setUp(self):
         '''
@@ -30,3 +30,24 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.last_name,"lin")
         self.assertEqual(self.new_user.user_name,"lin@lin")
         self.assertEqual(self.new_user.password,"12345")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the use_list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+
+    # def test_save_multiple_users(self):
+    #     '''
+    #     a test that checks whether both values appended to the array are actually present\ and returns the acount itself
+    #     '''
+    #     self.new_user.save_account()
+    #     test_userAc = User('er', 'ro', 'ro@ro', '234')
+    #     test_userAc.save_account()
+    #     self.assertEqual(len(User.user_list), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
