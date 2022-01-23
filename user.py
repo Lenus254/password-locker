f5c5ef1be48087701e540c392901c33c06af6af9
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-01-23 06:27:23 +0300

    imported unitttest and user to user_test account

--- user.py
+++ user.py
@@ -4,3 +4,12 @@ class User:
     """
 
 user_list=[] #Empty list to store users
+
+def __init__(self,first_name,last_name,user_name,password):
+
+      """instance of a class"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_number = user_name
+        self.password = password
