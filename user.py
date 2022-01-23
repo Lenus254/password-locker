c282771ade5d6db8dc4c9ca3848772a0d613fcc3
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-01-23 06:49:34 +0300

    created user objectfor running test cases

--- user.py
+++ user.py
@@ -7,9 +7,9 @@ user_list=[] #Empty list to store users
 
 def __init__(self,first_name,last_name,user_name,password):
 
-      """instance of a class"""
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.user_number = user_name
-        self.password = password
+    """instance of a class"""
+    
+    self.first_name = first_name
+    self.last_name = last_name
+    self.user_number = user_name
+    self.password = password
--- user_test.py
+++ user_test.py
@@ -1,2 +1,14 @@
 import unittest
 from user import User
+
+class TestUser(unittest.TestCase):
+    """define test case for user class behavior"""
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+    def tearDown(self):
+        User.user_list = []
+
+         self.new_user = User("Linus","lin","lin@lin","12345") # create user object for test
