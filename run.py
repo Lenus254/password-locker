ad04ea7b5932e78ef66e88b27068a42762eb5ebc
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-01-24 19:34:38 +0300

    modified main run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3.9
+from ast import Break
+from signal import siginterrupt
 from user import User
 from credentials import Credentials
 
@@ -40,75 +42,24 @@ def display_credentials():
 def delete_credentials(credentials):
     credentials.delete_credentials()
 
+def confirm_user(first_name,password):
+	'''
+	Function that verifies the existance of the user before creating credentials
+	'''
+	c = User.check_user(first_name,password)
+	return c
+
 def main():
-    # print('WELCOME TO PASSLOCK')
-    print('Use the following numbers to pick their corresponding values')
+    # print('SITES PASSWORD MANAGER')
+    print('Select from letters below for corresponding actions:')
     while True:
 
-        print(" 1) LOGIN \n 2) SIGN UP \n 3) ABOUT PASSLOCK \n 4) DISPLAY ACCOUNTS \n 5) SIGN OUT")
+        print(" A) LOGIN \n B) CREATE ACCOUNT \n C) ABOUT SITE \n D) DISPLAY USERS \n E) LOGOUT")
 
-        choice = int(input())
-        if choice == 1:
-            print('Enter username')
-            username = input()
-            print('Enter passoword')
-            password = input()
-            account = find_accounts(username)
-            if account.user_name == username and account.password == password:
-
-                print('logged in ')
-                while True:
-
-                    print(
-                        f'Welcome {username}, Use the following numbers to select their corresponding          values')
-
-                    print(
-                        ' 1) Save new password \n 2) Delete password \n 3) Display saved passwords \n 4) Log out ')
-
-                    log_choice = int(input())
-                    if log_choice == 1:
-                        print('New page')
-                        print('*'*100)
-
-                        print('Page name')
-                        page = input()
-
-                        print('password')
-                        password = input()
-
-                    # created and saved page
-                        save_page(create_page(page, password))
-
-                    elif log_choice == 2:
-                        print("Enter the name of the page you want to delete")
-
-                        page = input()
-                        if isexist_page(page):
-                            remove_page = (page)
-                            delete_page(remove_page)
-
-                        else:
-                            print(f'I cant find {page}')
-
-                    elif log_choice == 3:
-                        if display_pages():
-                            for pag in display_pages():
-                                print(
-                                    f'{pag.page}:{pag.password}'
-                                )
-                        else:
-                            print('NO PASSWORD SAVED YET')
-                            print('\n')
-
-                    elif log_choice == 4:
-                        print('adios')
-                        break
-            else:
-                print('wrong credentials')
-
-        if choice == 2:
+        option = str(input())
+        if option == 'B':
             print('NEW ACCOUNT')
-            print('*'*100)
+            print('-'*60)
 
             print('FIRSTNAME')
             first_name = input()
@@ -122,72 +73,136 @@ def main():
             print('PASSWORD')
             password = input()
 
-            save_account(create_account(
+            save_users(create_user(
                 first_name, last_name, user_name, password))
-            # create and save a new account
-            print('ACCOUNT CREATED')
-            while True:
+            # create and save a new user
+            print('User created succesfully')
+            
+        elif option =='A':
+            print('Enter username')
+            username = input()
+            print('Enter passoword')
+            password = input()
+            user = confirm_user(first_name,password)
+            if user == username:
 
-                print(
-                    f'Welcome {user_name}, Use the following numbers to select their corresponding values')
-                print(
-                    ' 1) Save new password \n 2) Delete password \n 3) Display saved passwords \n 4) Log out ')
+                print('User logged in ')
+            else:
+                print('Invalid user credentials')
+                break
+                
+                while True:
 
-                log_choice = int(input())
-                if log_choice == 1:
-                    print('New page')
-                    print('*'*100)
+                    print(
+                        f'Welcome {username}, Use the following numbers to select their corresponding options')
 
-                    print('Page name')
-                    page = input()
+                    print(
+                        ' 1) Save new credential \n 2) Delete credential \n 3) Display saved credentials \n 4) check out ')
 
-                    print('password')
-                    password = input()
+                    login_option = int(input())
+                    if login_option == 1:
+                        print('New credential')
+                        print('-'*80)
 
-                    # created and saved page
-                    save_page(create_page(page, password))
+                        print(' Enter site name')
+                        site_name = input()
 
-                elif log_choice == 2:
-                    print("Enter the name of the page you want to delete")
+                        print('Enter password')
+                        password = input()
 
-                    page = input()
-                    if isexist_page(page):
-                        remove_page = (page)
-                        delete_page(remove_page)
+                    
+                        save_site(create_credentials(site_name, password))
 
-                    else:
-                        print(f'I cant find {page}')
+                    elif login_option == 2:
+                        print("Enter the name of the site you want to delete")
 
-                elif log_choice == 3:
-                    if display_pages():
-                        for pag in display_pages():
-                            print(
-                                f'{pag.page}:{pag.password}'
-                            )
-                    else:
-                        print('NO PASSWORD SAVED YET')
+                        site_name = input()
+                        if credentials_exists(site_name):
+                            remove_site = (site_name)
+                            delete_credentials(remove_site)
 
-                elif log_choice == 4:
-                    break
+                        else:
+                            print(f' {site_name} not found')
 
-        elif choice == 3:
-            print('ABOUT PASSLOCK')
+                    elif login_option == 3:
+                        if display_credentials():
+                            for user in display_credentials():
+                                print(
+                                    f'{user.site_name}:{user.password}'
+                                )
+                        else:
+                            print('You have no credentials so far')
+                            print('\n')
+
+                    elif login_option == 4:
+                        print('no credentials to display')
+                        break
+            # while True:
+
+            #     print(
+            #         f'Welcome {user_name}, Use the following numbers to select their corresponding values')
+            #     print(
+            #         ' 1) Save new password \n 2) Delete password \n 3) Display saved passwords \n 4) Log out ')
+
+            #     log_choice = int(input())
+            #     if log_choice == 1:
+            #         print('New page')
+            #         print('*'*100)
+
+            #         print('Page name')
+            #         page = input()
+
+            #         print('password')
+            #         password = input()
+
+            #         # created and saved page
+            #         save_page(create_page(page, password))
+
+            #     elif log_choice == 2:
+            #         print("Enter the name of the page you want to delete")
+
+            #         page = input()
+            #         if isexist_page(page):
+            #             remove_page = (page)
+            #             delete_page(remove_page)
+
+            #         else:
+            #             print(f'I cant find {page}')
+
+            #     elif log_choice == 3:
+            #         if display_pages():
+            #             for pag in display_pages():
+            #                 print(
+            #                     f'{pag.page}:{pag.password}'
+            #                 )
+            #         else:
+            #             print('NO PASSWORD SAVED YET')
+
+            #     elif log_choice == 4:
+            #         break
+            
+            # else:
+            #     print('wrong credentials')
+            
+        
+        elif option == 'C':
+            print('ABOUT THE SITE')
             print(
                 '''
-            Passlock is an sort of script application that allows you to store  password from different ccounts. In case of many accounts on social media passlock can be used to store the different password from the social media  accounts.Instead of having to use one password for all your sites so that you can remember  easily,you can use different password and store them in passlock and only have to remember your passlock password. This can prove to be very helpful especially  against hackers.
-                                    ''')
+            Passlock is a terminal application that allows a user  credentials from different sites. In case of many sites like social media and others , passlock can be used to save different login credentials from the social media  accounts and other sites.Instead of having to gram credentials for all your sites so that you can remember  easily,you can use different login credentials and save them in passlock and only have to remember your passlock user account details only. This can prove to be very helpful especially  if you have many accounts.
+                ''')
 
-        elif choice == 4:
-            if display_accounts():
-                for account in display_accounts():
+        elif option == 'D':
+            if display_users():
+                for user in display_users():
                     print(
-                        f'{account.user_name}'
+                        f'{user.user_name}'
                     )
             else:
                 print('NO ACCOUNTS')
 
-        elif choice == 5:
-            print('adios')
+        elif option == 'E':
+            print('EXIT')
             break
 
 
--- user.py
+++ user.py
@@ -1,20 +1,21 @@
+from ast import If
+
+
 class User:
     """
     Class that generates new instances of users.
     """
 
-    user_list=[] #Empty list to store users
+    user_list = []  # Empty list to store users
 
-    def __init__(self,first_name,last_name,user_name,password):
-
+    def __init__(self, first_name, last_name, user_name, password):
         """instance of a class"""
-    
+
         self.first_name = first_name
         self.last_name = last_name
         self.user_name = user_name
         self.password = password
 
-    
     def save_user(self):
         '''
         this is a save function that appends the account to the user_accounts array
@@ -22,12 +23,11 @@ class User:
         User.user_list.append(self)
 
     def delete_user(self):
-
         '''
         delete_contact method deletes a saved user from the user_list
         '''
 
-        User.user_list.remove(self)  
+        User.user_list.remove(self)
 
     @classmethod
     def search_by_user_name(cls, user_name):
@@ -36,10 +36,10 @@ class User:
         '''
         for user in cls.user_list:
             if user.user_name == user_name:
-                return user   
+                return user
 
     @classmethod
-    def user_exist(cls,user_name):
+    def user_exist(cls, user_name):
         '''
         Method that checks if a user exists from the user list.
         Args:
@@ -54,5 +54,16 @@ class User:
         return False
 
     @classmethod
+    def check_user(cls, first_name, password):
+        current_user = ''
+        for user in User.user_list:
+            if(user.first_name == first_name and user.password == password):
+                current_user = user.first_name
+                return current_user
+
+    @classmethod
     def display_users(cls):
         return cls.user_list
+
+
+    
